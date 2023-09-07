c7e70559386317111109787adaafb2d58821156a
Jose Zaccaro <a.zaccaro42@gmail.com>
2023-09-07 18:47:49 -0300

    conflictos

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,17 +1,1 @@
-
-import User from './../models/User.js';
-
-async function getUsers(req, res, next) {
-    try {
-        const allUsers = await User.find()
-        res.json({
-            response: "allUsers"
-        });
-    } catch (error) {
-
-    }
-}
-
-
-
-export { getUsers }
+asdasd
