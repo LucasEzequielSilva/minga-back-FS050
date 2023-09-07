d989651affb5e863c95bfbb280f2a3b94ab0dc82
Jose Zaccaro <a.zaccaro42@gmail.com>
2023-09-07 18:46:49 -0300

    forzando conflicto

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -5,10 +5,8 @@ async function getUsers(req, res, next) {
     try {
         const allUsers = await User.find()
         res.json({
-            response: allUsers
+            response: "allUsers"
         });
-
-        console.log('hice una peticion')
     } catch (error) {
 
     }
